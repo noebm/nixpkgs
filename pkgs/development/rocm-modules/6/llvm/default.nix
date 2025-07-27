c02bf888346db6fd9ae345284a18c62a4ca82297
Moritz Noebauer <moritz.noebauer@gmail.com>
2025-07-27 11:20:54 +0200

    rocm-llvm: remove disabled patches in llvm build

--- pkgs/development/rocm-modules/6/llvm/default.nix
+++ pkgs/development/rocm-modules/6/llvm/default.nix
@@ -28,12 +28,6 @@
 }:
 
 let
-  filter-patches =
-    excluded-patches: patches:
-    builtins.filter (
-      x: !builtins.any (patch: (lib.strings.hasSuffix patch (builtins.baseNameOf x))) excluded-patches
-    ) patches;
-
   llvmPackagesNoBintools = llvmPackages_19.override {
     bootBintools = null;
     bootBintoolsNoLibc = null;
@@ -442,33 +436,24 @@ rec {
   clang-tools = llvmPackagesRocm.clang-tools.override {
     inherit clang-unwrapped clang;
   };
-  compiler-rt-libc = llvmPackagesRocm.compiler-rt-libc.overrideAttrs (
-    old:
-    let
-      excluded-patches = [
-        "armv6-scudo-libatomic.patch"
-        "14ae0a660a38e1feb151928a14f35ff0f4487351.patch"
-      ];
-    in
-    {
-      patches = filter-patches excluded-patches old.patches ++ [
-        (fetchpatch {
-          name = "Fix-missing-main-function-in-float16-bfloat16-support-checks.patch";
-          url = "https://github.com/ROCm/llvm-project/commit/68d8b3846ab1e6550910f2a9a685690eee558af2.patch";
-          hash = "sha256-Db+L1HFMWVj4CrofsGbn5lnMoCzEcU+7q12KKFb17/g=";
-          relative = "compiler-rt";
-        })
-        # GLIBC 2.40 cause build failures due to overloads. See https://github.com/llvm/llvm-project/issues/100754.
-        # This adds the PR for https://github.com/llvm/llvm-project/pull/100986/
-        (fetchpatch {
-          name = "Avoid-overload-ambiguity-for-interceptors.patch";
-          url = "https://github.com/llvm/llvm-project/commit/256149db05b803f22e7baadbccb8c0c097c7ee27.patch";
-          hash = "sha256-pgpN1q1vIQrPXHPxNSZ6zfgV2EflHO5Amzl+2BDjXbs=";
-          relative = "compiler-rt";
-        })
-      ];
-    }
-  );
+  compiler-rt-libc = llvmPackagesRocm.compiler-rt-libc.overrideAttrs (old: {
+    patches = old.patches ++ [
+      (fetchpatch {
+        name = "Fix-missing-main-function-in-float16-bfloat16-support-checks.patch";
+        url = "https://github.com/ROCm/llvm-project/commit/68d8b3846ab1e6550910f2a9a685690eee558af2.patch";
+        hash = "sha256-Db+L1HFMWVj4CrofsGbn5lnMoCzEcU+7q12KKFb17/g=";
+        relative = "compiler-rt";
+      })
+      # GLIBC 2.40 cause build failures due to overloads. See https://github.com/llvm/llvm-project/issues/100754.
+      # This adds the PR for https://github.com/llvm/llvm-project/pull/100986/
+      (fetchpatch {
+        name = "Avoid-overload-ambiguity-for-interceptors.patch";
+        url = "https://github.com/llvm/llvm-project/commit/256149db05b803f22e7baadbccb8c0c097c7ee27.patch";
+        hash = "sha256-pgpN1q1vIQrPXHPxNSZ6zfgV2EflHO5Amzl+2BDjXbs=";
+        relative = "compiler-rt";
+      })
+    ];
+  });
   compiler-rt = compiler-rt-libc;
   bintools = wrapBintoolsWith {
     bintools = llvmPackagesRocm.bintools-unwrapped.override {
