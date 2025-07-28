53222f453821399d0cd54a0868f73988ef35ba65
Moritz Noebauer <moritz.noebauer@gmail.com>
2025-07-28 14:13:04 +0200

    amdsmi: update version

--- pkgs/development/rocm-modules/6/amdsmi/default.nix
+++ pkgs/development/rocm-modules/6/amdsmi/default.nix
@@ -21,12 +21,12 @@ let
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "amdsmi";
-  version = "6.3.3";
+  version = "6.4.2";
   src = fetchFromGitHub {
     owner = "rocm";
     repo = "amdsmi";
     rev = "rocm-${finalAttrs.version}";
-    hash = "sha256-hrPqd4ZWqzTG7JRfVwc1SZx6TNS0Q/LFg8yDxrM3mPo=";
+    hash = "sha256-9O29O4HGkQxFDglAhHKv5KWA7p97RwMGG2x/fkOS2jE=";
   };
 
   postPatch = ''
