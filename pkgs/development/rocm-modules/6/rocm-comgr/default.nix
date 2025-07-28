0f674c82284dda2d6246fef943619ec2003bb49e
Moritz Noebauer <moritz.noebauer@gmail.com>
2025-07-28 12:06:34 +0200

    rocm-comgr: remove incompatible patch

--- pkgs/development/rocm-modules/6/rocm-comgr/default.nix
+++ pkgs/development/rocm-modules/6/rocm-comgr/default.nix
@@ -35,12 +35,6 @@ stdenv.mkDerivation (finalAttrs: {
       url = "https://github.com/GZGavinZhao/rocm-llvm-project/commit/a439e4f37ce71de48d4a979594276e3be0e6278f.patch";
       relative = "amd/comgr";
     })
-    #[Comgr] Extend ISA compatibility for CCOB
-    (fetchpatch {
-      sha256 = "sha256-6Rwz12Lk4R2JK3olii3cr2Zd0ZLYe7VSpK1YRCOsJWY=";
-      url = "https://github.com/GZGavinZhao/rocm-llvm-project/commit/2d8c459a4d4c0567a7a275b4b54560d88e5c6919.patch";
-      relative = "amd/comgr";
-    })
   ];
 
   nativeBuildInputs = [
